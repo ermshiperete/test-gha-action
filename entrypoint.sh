15ce44a212becc341d612f9e164a79e1e7e79e9e
Eberhard Beilharz <eb1@sil.org>
2023-06-22 17:24:51 +0200

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 IMAGE_NAME="$1"
 
 cd "${INPUT_SOURCE_DIR}" || exit
-docker run -v "$(pwd)":/source -i -t -w /source --platform="linux/${INPUT_PLATFORM}" \
+docker run -v "$(pwd)":/source --workdir /source --platform="linux/${INPUT_PLATFORM}" \
     "${IMAGE_NAME}" "${INPUT_DIST}" "${INPUT_SOURCEPACKAGE}" .
