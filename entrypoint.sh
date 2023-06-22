71447b4a20a923fa57430682f2ddc3b5e1b397e0
Eberhard Beilharz <eb1@sil.org>
2023-06-22 17:53:50 +0200

    `env` instead of `with`

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-echo "***** in entrypoint.sh: $*"
+echo "::notice::***** in entrypoint.sh: $*"
 env
 cd "${INPUT_SOURCE_DIR}" || exit
 docker run -v "$(pwd)":/source --workdir /source --platform="linux/${INPUT_PLATFORM}" \
