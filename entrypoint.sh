da140514d459bf08b3939cbc7010e78dd76cd715
Eberhard Beilharz <eb1@sil.org>
2023-06-22 18:07:07 +0200

    Fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,5 +3,5 @@
 echo "::notice::***** in entrypoint.sh: $*"
 env
 cd "${INPUT_SOURCE_DIR}" || exit
-docker run -v "$(pwd)":/source --workdir /source --platform="linux/${INPUT_PLATFORM}" \
+docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$(pwd)":/source --workdir /source --platform="linux/${INPUT_PLATFORM}" \
     "${INPUT_IMAGE}" "${INPUT_DIST}" "${INPUT_SOURCEPACKAGE}" .
