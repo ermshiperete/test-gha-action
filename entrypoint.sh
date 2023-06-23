47dd0382768a344cd3c1a3fceb9dd8caae4355d5
Eberhard Beilharz <eb1@sil.org>
2023-06-23 12:51:49 +0200

    Directly use run command instead of entrypoint script

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "::notice::***** in entrypoint.sh: $*"
-env
-cd "${INPUT_SOURCE_DIR}" || exit
-docker run -v "/var/run/docker.sock":"/var/run/docker.sock" -v "$(pwd)":/source --workdir /source --platform="linux/${INPUT_PLATFORM}" \
-    "${INPUT_IMAGE}" "${INPUT_DIST}" "${INPUT_SOURCEPACKAGE}" .
